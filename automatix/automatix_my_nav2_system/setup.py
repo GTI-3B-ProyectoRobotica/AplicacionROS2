b4c0b62c7cb4a47905e5d8690e2f3a656bbb8405
rubenpardo <rubenpardocasanova@gmail.com>
2022-03-17 17:50:54 +0100

    AX - H1 - 8 - Navegacion automatica finalizada 17/08/2022
    uwu

--- automatix/automatix_my_nav2_system/setup.py
+++ automatix/automatix_my_nav2_system/setup.py
@@ -13,12 +13,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'map'), glob('map/*.pgm')),
-        (os.path.join('share', package_name, 'map'), glob('map/*.yaml')),
-        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),  
-        (os.path.join('share', package_name, 'config'), glob('config/*.lua')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.pgm')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.rviz')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.xml')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.model'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -29,7 +29,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'initial_pose_pub = automatix_my_nav2_system.initial_pose_pub:main' #añadir
+            'initial_pose_pub = automatix_my_nav2_system.initial_pose_pub:main',
+            'load_map = automatix_my_nav2_system.load_map:main',
+            'nav_to_pose = automatix_my_nav2_system.nav_to_pose:main',
+            'go_to_pose = automatix_my_nav2_system.go_to_pose:main'
         ],
     },
 )
