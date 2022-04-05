f78ab00114e17e870bc0f25cfd3b6428a60ad894
Lorelay <rainbowg03@gmail.com>
2022-04-05 19:51:12 +0200

    AX- H4 - 2 Servicio Ir a Zona 05/04/2022
    lectura de un txt de la zona y creación de zona

--- automatix/automatix_nav_zona/automatix_nav_zona/nav_zona_server.py
+++ automatix/automatix_nav_zona/automatix_nav_zona/nav_zona_server.py
@@ -5,37 +5,46 @@ from automatix_custom_interface.srv import IrZona
 #importar  biblioteca Python ROS2
 import rclpy
 from rclpy.node import Node
+import sys
+sys.path.insert(1, '../automatix_nav_zona')
+from automatix_nav_zona.zona import Zona
 
 class Service(Node):
 
-    zonas = {
-        'zona1': ['0.0', '0.0'],
-        'zona2': ['1.0', '1.0'],
-        'zona3': ['2.0', '2.0']
-    }
+    _zonas = {}
 
     def __init__(self):
         #constructor con el nombre del nodo
-        super().__init__('nav_zona_server') 
+        super().__init__('nav_zona_server')
+        #cambiarlo por ruta relativa
+        self._rellenarDiccionarioZonas("/home/lorelay/turtlebot3_ws/src/AplicacionROS2/automatix/zonas/zonas.txt")
+        
         # declara el objeto servicio pasando como parametros
         # tipo de mensaje
         # nombre del servicio
         # callback del servicio
         self.srv = self.create_service(IrZona, 'IrZona', self.automatix_nav_zona_callback)
 
-        #declara el objeto publisher pasando como parametros
-        # tipo de mensaje
-        # nombre del topic
-        # tamaño de la cola
-
-        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
 
     def automatix_nav_zona_callback(self, request, response):
-        
-        self.get_logger().info(request.zona)
-        response.success = True
+        #comprobamos que request.zona (lo que mandamos) existe en el diccionario
+        if request.zona in self._zonas:
+            self.get_logger().info(self._zonas[request.zona].toString())
+            response.success = True
+        else:
+            response.success = False
         # devuelve la respuesta
         return response
+    
+    def _rellenarDiccionarioZonas(self, file):
+        """Metodo para rellenar el diccionario con zonas
+        Args:
+            file: nombre del fichero donde están guardadas las zonas    
+        """
+        arrayZonas = Zona.zonaFromTxt(file)
+        for zona in arrayZonas:
+            self._zonas[zona.get_name()] = zona
+
 
 def main(args=None):
     # inicializa la comunicacion ROS2
---
+++ automatix/automatix_nav_zona/automatix_nav_zona/zona.py
@@ -0,0 +1,93 @@
+import re
+
+from numpy import double
+
+"""
+Este fichero contiene la definición para poder representar una Zona
+Classes: 
+    Zona
+
+Author: Lorelay Pricop
+Version: 04/04/2022
+"""
+
+class Zona:
+    """
+    Representa una zona definida por el usuario. Las coordenadas están guardadas en un txt
+    
+    Attributes:
+        _name: Nombre de la zona
+        _xi: x inferior
+        _yi: y inferior
+        _xs: x superior
+        _ys: y superior
+        _punto_medio: punto medio de la zona
+
+    Methods:
+        _calcular_punto_medio(): calcula el punto medio de dos coordenadas
+        get_name(): devuelve el nombre de la zona
+        toString(): devuelve los atributos del objeto como un string
+        zonaFromTxt(file): devuelve las zonas formateadas desde un txt
+    
+    """
+    def __init__(self, linea):
+        """ Inicializa la zona
+        
+        Args: 
+            linea: nombre y coordenadas fomateadas como "zona1:xi$yi$xs$ys"
+        """
+        array1 = linea.split(':')
+        name = array1[0]
+        coordenadas = array1[1].split(',')
+        self._name=name
+        self._xi=coordenadas[0]
+        self._yi=coordenadas[1]
+        self._xs=coordenadas[2]
+        self._ys=coordenadas[3]
+        self._punto_medio = self._calcular_punto_medio()
+
+
+    def get_name(self):
+        """Devuelve el nombre de la zona
+        Return: 
+            name: el nombre de la zona"""
+        return self._name
+
+    
+    def toString(self):
+        return self._name+": inferior(" +self._xi +  ", " + self._yi +  "), superior(" + self._xs +  ", " + self._ys +  "), " + "Punto medio: " + str(self._punto_medio) 
+    
+    def _calcular_punto_medio(self):
+        """ Calcula el punto medio de la zona
+        Return:
+            x, y: las coordenadas x e y del punto medio
+        """
+
+        x = (double(self._xi)+double(self._xs))/2
+        y = (double(self._yi)+double(self._ys))/2
+
+        return x,y
+
+
+    @staticmethod
+    def zonaFromTxt(file):
+        """Lee de un txt las zonas y las convierte en un array
+
+        Args:
+            file: el nombre del fichero
+
+        Return:
+            arrayZonas: array de linea por zona, separados por ; en el txt
+        """
+        lines = ""
+        arrayZonas = []
+        with open(file) as f:
+            lines = f.read()
+
+        arrayZonasString = lines.split(';') 
+        
+        for zonaString in arrayZonasString:
+            if len(zonaString)>2:
+                arrayZonas.append(Zona(zonaString))
+
+        return arrayZonas
